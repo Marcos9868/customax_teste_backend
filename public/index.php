39c8be12e87f19d8f56a7cd3ddd7a7a6c6165615
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-04-22 17:27:11 -0300

    Fix: comment user routes

--- public/index.php
+++ public/index.php
@@ -3,4 +3,4 @@
 require_once '../vendor/autoload.php';
 require_once '../src/config/Database.php';
 require_once '../src/routes/TaskRoute.php';
-require_once '../src/routes/UserRoute.php';
+// require_once '../src/routes/UserRoute.php';
