9f902efe3e2dafb62ed5b58dd4bc292a7f89edf3
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-04-22 15:10:03 -0300

    Fix: change way to connect a mysql on application

--- src/config/db.php
+++ src/config/db.php
@@ -1,16 +1,35 @@
 <?php
+namespace Src\Config;
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__, "/../");
-$dotenv->load();
-$host = 'localhost';
-$dbname = getenv("MYSQL_DB");
-$username = getenv("MYSQL_USERNAME");
-$password = getenv("MYSQL_PASSWORD");
+use PDO;
+use PDOException;
+use Dotenv\Dotenv;
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erro de conexão: " . $e->getMessage());
+class Database {
+    private static $pdo = null;
+
+    public static function getConnection() {
+        if (self::$pdo === null) {
+            $dotenv = Dotenv::createImmutable(__DIR__, "/../");
+            $dotenv->load();
+
+            $host = 'localhost';
+            $dbname = getenv("MYSQL_DB");
+            $username = getenv("MYSQL_USERNAME");
+            $password = getenv("MYSQL_PASSWORD");
+
+            try {
+                // Criar a instância PDO
+                self::$pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+                self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                self::$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);  // Garantir fetch associativo
+            } catch (PDOException $e) {
+                // Em caso de erro, exibe a mensagem
+                die("Erro de conexão: " . $e->getMessage());
+            }
+        }
+        
+        return self::$pdo;
+    }
 }
 ?>
